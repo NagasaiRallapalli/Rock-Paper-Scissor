050e064b5e6be51c932abc42765d67b6fa655f82
NagasaiRallapalli <phanirajrallapalli@gmail.com>
2026-05-28 11:57:11 +0530

    new-update

--- script1.js
+++ script1.js
@@ -1,80 +1,46 @@
-let Allimg=document.querySelectorAll('.imagecontainer img');
-
+let Allimg = document.querySelectorAll('.imagecontainer img');
 let userChoice;
 let compChoice;
+let userScore = 0;
+let compScore = 0;
+let btn = document.getElementById('btn');
+let userScoreEl = document.getElementById('userscore');
+let compScoreEl = document.getElementById('compscore');
 
-let userScore=0;
-let compScore=0;
-
-let btn=document.getElementById('btn');
-let userscore=document.getElementById('userscore');
-let compscore=document.getElementById('compscore');
-
-
-function compChoicefunc(){
-    let arr=["rock","paper","scissor"];
-    let indexarr=Math.floor(Math.random()*3);
+function compChoicefunc() {
+    let arr = ["rock", "paper", "scissor"];
+    let indexarr = Math.floor(Math.random() * 3);
     return arr[indexarr];
 }
-function gameimplementation(uchoice){
-    compChoice=compChoicefunc();
 
-    document.getElementById('displaypara').innerHTML=`User selected : ${uchoice} and Computer Selected ${compChoice}`;
+function gameimplementation(uchoice) {
+    compChoice = compChoicefunc();
 
+    document.getElementById('displaypara').innerHTML =
+        `User selected : ${uchoice} and Computer Selected ${compChoice}`;
 
-    if(compChoice===userChoice ){
-        btn.innerHTML="Game Draw!!"
-        Round--;
+    if (compChoice === uchoice) {
+        btn.innerHTML = "Game Draw!!";
     }
-    else if(compChoice==='rock' && userChoice==='scissor'){
-        btn.innerHTML="Comp Won!!"
+    else if (
+        (compChoice === 'rock'    && uchoice === 'scissor') ||
+        (compChoice === 'paper'   && uchoice === 'rock')    ||
+        (compChoice === 'scissor' && uchoice === 'paper')
+    ) {
+        btn.innerHTML = "Comp Won!!";
         compScore++;
-        compScore.innerHTML=compScore;
-        Round--;
-
+        compScoreEl.innerHTML = compScore;
     }
-    else if(compChoice==='scissor' && userChoice==='rock'){
-        btn.innerHTML="User Won!!"
+    else {
+        btn.innerHTML = "User Won!!";
         userScore++;
-        userscore.innerHTML=userScore;
-        Round--;
-    }
-    else if(compChoice==='paper' && userChoice==='rock'){
-        btn.innerHTML="Comp Won!!"
-        compScore++;
-        compScore.innerHTML=compScore;
-        Round--;
-    }
-    else if(compChoice==='rock' && userChoice==='paper'){
-        btn.innerHTML="User Won!!"
-        userScore++;
-        userscore.innerHTML=userScore;
-        Round--;
-
-    }
-    else if(compChoice==='scissor' && userChoice==='paper'){
-        btn.innerHTML="Comp Won!!"
-        compScore++;
-        compscore.innerHTML=compScore;
-        Round--;
-    }
-    else if(compChoice==='paper' && userChoice==='scissor'){
-        btn.innerHTML="User Won!!"
-        userScore++;
-        userscore.innerHTML=userScore;
-        Round--;
-
-    }
-    else{
-        btn.innerHTML="Invalid !!"
-
+        userScoreEl.innerHTML = userScore;
     }
 }
-Allimg.forEach((i)=>{
-    i.addEventListener('click',()=>{
-        userChoice=i.id;
-        gameimplementation(userChoice)
-       
-    })
-})
 
+Allimg.forEach((i) => {
+    i.addEventListener('click', () => {
+        userChoice = i.id;
+        gameimplementation(userChoice);
+    });
+});
